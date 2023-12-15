67a5a6bbc3462f92701fa8b29cc2dea9d23ce179
lmadiaz84 <lma_diaz@hotmail.com>
2023-12-15 17:15:52 -0300

    se creo instancia flask

--- src/app.py
+++ src/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import render_template
+from flaskext.mysql import mySQL
 
 app = Flask(__name__)
+mysql = mySQL()
 
+if __name__=='__main__':
+    app.run(debug=True)
+
+""" comentado
 @app.route("/hola")
 def hello():
     return "<p>Hola, Mundo! App</p>"
@@ -13,3 +20,5 @@ def bye():
 @app.route("/contacto")
 def contactoHandling():
     return "contacto guardado"
+    
+"""
