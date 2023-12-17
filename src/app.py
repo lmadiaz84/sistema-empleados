e344b41d2736ba387304352fb746f90aeef78cf1
lmadiaz84 <lma_diaz@hotmail.com>
2023-12-17 16:30:25 -0300

    modificacion app,index y alta database

--- src/app.py
+++ src/app.py
@@ -1,24 +1,64 @@
-from flask import Flask
-from flask import render_template
-from flaskext.mysql import mySQL
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import database as db
 
-app = Flask(__name__)
-mysql = mySQL()
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'src', 'templates')
+
+app = Flask(__name__, template_folder = template_dir) 
+
+#Rutas de la aplicación
+@app.route('/')
+def home():
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM empleados")
+    myresult = cursor.fetchall()
+    #Convertir los datos a diccionario
+    insertObject = []
+    columnNames = [column[0] for column in cursor.description]
+    for record in myresult:
+        insertObject.append(dict(zip(columnNames, record)))
+    cursor.close()
+    return render_template('index.html', data=insertObject)
+  
+#Ruta para guardar usuarios en la bdd
+@app.route('/user', methods=['POST'])
+def addUser():
+    nombre = request.form['nombre']
+    correo = request.form['correo']
+    foto = request.form['foto']
+
+    if nombre and correo and foto:
+        cursor = db.database.cursor()
+        sql = "INSERT INTO `empleados` (`nombre`, `correo`, `foto`) VALUES (%s, %s, %s)"
+        data = (nombre, correo, foto)
+        cursor.execute(sql, data)
+        db.database.commit()
+    return redirect(url_for('home'))  
+
+@app.route('/delete/<string:id>')
+def delete(id):
+    cursor = db.database.cursor()
+    sql = "DELETE FROM empleados WHERE id=%s"
+    data = (id,)
+    cursor.execute(sql, data)
+    db.database.commit()
+    return redirect(url_for('home'))
+
+@app.route('/edit/<string:id>', methods=['POST'])
+def edit(id):
+    nombre = request.form['nombre']
+    correo = request.form['correo']
+    foto = request.form['foto']
+
+    if nombre and correo and foto:
+        cursor = db.database.cursor()
+        sql = "UPDATE empleados SET nombre = %s, correo = %s, foto = %s WHERE id = %s"
+        data = (nombre, correo, foto, id)
+        cursor.execute(sql, data)
+        db.database.commit()
+    return redirect(url_for('home'))
 
 if __name__=='__main__':
     app.run(debug=True)
-
-""" comentado
-@app.route("/hola")
-def hello():
-    return "<p>Hola, Mundo! App</p>"
-
-@app.route("/chau")
-def bye():
-    return "<p>chau, Mundo!</p>"
-
-@app.route("/contacto")
-def contactoHandling():
-    return "contacto guardado"
-    
-"""
+ 
